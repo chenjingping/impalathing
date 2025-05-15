f8deca32d88777542d8f257f044a7eea99d043a5
cjpvscjp <cjpvscjp@gmail.com>
2025-05-15 18:22:35 +0800

    Update hive_metastore-consts.go

--- services/hive_metastore/hive_metastore-consts.go
+++ services/hive_metastore/hive_metastore-consts.go
@@ -6,7 +6,7 @@ package hive_metastore
 import (
 	"bytes"
 	"fmt"
-	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/chenjingping/thrift/lib/go/thrift"
 	"github.com/chenjingping/impalathing/services/fb303"
 
 )
