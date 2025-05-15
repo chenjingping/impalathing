02b88edba79e5a9bffe87cc74dca02c4efcd84d8
cjpvscjp <cjpvscjp@gmail.com>
2025-05-15 18:24:01 +0800

    Update impala_service-remote.go

--- services/impalaservice/impala_service-remote/impala_service-remote.go
+++ services/impalaservice/impala_service-remote/impala_service-remote.go
@@ -12,10 +12,10 @@ import (
         "os"
         "strconv"
         "strings"
-        "git.apache.org/thrift.git/lib/go/thrift"
-	      "github.com/chenjingping/impalathing/services/status"
-	      "github.com/chenjingping/impalathing/services/beeswax"
-	      "github.com/chenjingping/impalathing/services/cli_service"
+        "github.com/chenjingping/thrift/lib/go/thrift"
+	"github.com/chenjingping/impalathing/services/status"
+	"github.com/chenjingping/impalathing/services/beeswax"
+	"github.com/chenjingping/impalathing/services/cli_service"
         "github.com/chenjingping/impalathing/services/impalaservice"
 )
 
