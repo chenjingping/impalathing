2bffb9de29f259189b3c96c809f458698ffba06f
cjpvscjp <cjpvscjp@gmail.com>
2025-05-15 18:19:08 +0800

    Update beeswax_service-remote.go

--- services/beeswax/beeswax_service-remote/beeswax_service-remote.go
+++ services/beeswax/beeswax_service-remote/beeswax_service-remote.go
@@ -13,7 +13,7 @@ import (
         "strconv"
         "strings"
         "github.com/chenjingping/thrift/lib/go/thrift"
-	      "github.com/chenjingping/impalathing/services/hive_metastore"
+	"github.com/chenjingping/impalathing/services/hive_metastore"
         "github.com/chenjingping/impalathing/services/beeswax"
 )
 
